212d5686a766d32da11bb1677651cbebf729e6dc
ChrisSu <suhao@espacetime.com>
2017-12-21 13:42:23 +0800

    Pluggable AI With Scriptable Objects

---
+++ Assets/Pluggable AI With Scriptable Objects/PluggableAI/Scripts/PatrolAction.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu (menuName = "PluggableAI/Actions/Patrol")]
+public class PatrolAction : Action {
+
+    public override void Act(StateController controller)
+    {
+        Patrol(controller);
+    }
+
+    private void Patrol(StateController controller)
+    {
+        controller.navMeshAgent.destination = controller.wayPointList[controller.nexWayPoint].position;
+        controller.navMeshAgent.Resume();
+
+        if (controller.navMeshAgent.remainingDistance <= controller.navMeshAgent.stoppingDistance 
+            && !controller.navMeshAgent.pathPending)
+        {
+            controller.nexWayPoint = (controller.nexWayPoint + 1) % controller.wayPointList.Count;
+        }
+    }
+}
